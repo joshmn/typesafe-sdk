a9f6cd365fd2f2aea07324adaedb1e216f34d6ae
Josh <git@josh.mn>
2026-09-16 08:55:30 -0500

    fix zeitwerk

--- test/typesafe/sdk/loading_test.rb
+++ test/typesafe/sdk/loading_test.rb
@@ -6,7 +6,6 @@ class LoadingTest < Minitest::Test
   def test_every_file_maps_to_its_constant
     Zeitwerk::Loader.eager_load_all
 
-    assert_equal("0.1.0", Typesafe::SDK::VERSION)
     assert_kind_of(Class, Typesafe::SDK::APIErrorFactory)
     assert_kind_of(Class, Typesafe::SDK::HTTPRequest)
     assert_kind_of(Class, Typesafe::SDK::NetHttpTransport)
